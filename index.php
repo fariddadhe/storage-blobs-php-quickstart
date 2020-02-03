72c8b1f4049fe0a0bf670f3cd6a7a6b7a4f95fc0
fariddadhe <60096170+fariddadhe@users.noreply.github.com>
2020-02-04 05:08:31 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     use MicrosoftAzure\Storage\Blob\Models\CreateContainerOptions;
     use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
     function getData(){
-        $connectionString = "DefaultEndpointsProtocol=https;AccountName=".getenv('ACCOUNT_NAME').";AccountKey=".getenv('ACCOUNT_KEY');
+        $connectionString = "DefaultEndpointsProtocol=https;AccountName=".getenv('azurewebblob').";AccountKey=".getenv('IRtUInLb2Cl8bYEp54JjFOsPTd9+eNdytEprSfLVTZWQKPp6i668ez1iU3h99iZRBjpqSsxwBi4dwHv7UvHLXg==');
 
         // Create blob client.
         $blobClient = BlobRestProxy::createBlobService($connectionString);
